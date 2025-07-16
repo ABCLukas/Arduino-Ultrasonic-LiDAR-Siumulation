2a43122a9b6dac0a4e780253e97321a62991d800
ABCLukas <schneiderluk548@gmail.com>
2025-07-16 14:44:13 +0200

    Add Quit Event Handler and compiled

---
+++ sdl.cpp
@@ -0,0 +1,68 @@
+//compile with:
+//g++ sdl.cpp -lSDL2 -o sdl
+
+#include<SDL2/SDL.h>
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+
+void drawPoint(int x, int y){
+	SDL_SetRenderDrawColor(renderer,255,255,255,255);
+	SDL_RenderDrawPoint(renderer,x,y);
+	SDL_RenderPresent(renderer);
+}
+
+void quitSDL(){
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int main (int argc, char *argv[]) {
+	if(SDL_Init(SDL_INIT_VIDEO)< 0){
+		std::cout<< "Video inilization ERROR:" << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+		window = SDL_CreateWindow("SDL_Test",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, WIDTH,HEIGHT,SDL_WINDOW_SHOWN); //other flats = ||
+	if(window == nullptr){
+		std::cout<< "Window creation ERROR: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+	
+	if(renderer == nullptr){
+		std::cout<<"Renderer creation ERROR: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_SetRenderDrawColor(renderer,0,0,0,255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	bool running = true;
+	SDL_Event event;
+
+	while(running){
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_QUIT){
+				running = false;
+			}
+		}
+	
+		SDL_Delay(16);
+	}
+
+	quitSDL();
+	return 0;
+}
