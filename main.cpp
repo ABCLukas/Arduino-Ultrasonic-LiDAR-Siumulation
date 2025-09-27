1a3487a60940ed0bbaa8f3237d29a59332dab051
Lukas <153985968+ABCLukas@users.noreply.github.com>
2025-09-27 16:08:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Points.hpp"
 
-// Declare the existing functions you implemented elsewhere
+//Declare the existing functions implemented elsewhere
 extern int startSDL(int argc, char* argv[]);
 extern int readSerialData();
 
 int main(int argc, char* argv[]) {
-    // Read data from Arduino (fills scannedPoints)
+    //Read data from Arduino (fills scannedPoints)
     if (readSerialData() != 0) {
         std::cerr << "Failed to read serial data.\n";
         return 1;
